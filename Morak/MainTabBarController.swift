b52cd29e51325fce1acb000068d0cd799957a22a
Hong Jeongmin <jeohong59@gmail.com>
2025-06-30 19:14:09 +0900

    [init] 모락 iOS 프로젝트 생성

---
+++ Morak/MainTabBarController.swift
@@ -0,0 +1,66 @@
+//
+//  MainTabBarController.swift
+//  Morak
+//
+//  Created by Hong jeongmin on 6/30/25.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupTabBar()
+        setupTabs()
+    }
+    
+    private func setupTabBar() {
+        tabBar.isTranslucent = false
+        tabBar.backgroundColor = .white
+        
+        let appearance = UITabBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .white
+        
+        tabBar.standardAppearance = appearance
+        tabBar.scrollEdgeAppearance = appearance
+    }
+    
+    private func setupTabs() {
+        let postVC = UIViewController()
+        let postNav = UINavigationController(rootViewController: postVC)
+        postNav.navigationBar.isHidden = true
+        postNav.tabBarItem = UITabBarItem(
+            title: "포스트",
+            image: UIImage(systemName: "text.document"),
+            tag: 0
+        )
+        
+        let chatVC = UIViewController()
+        chatVC.view.backgroundColor = .white
+        chatVC.tabBarItem = UITabBarItem(
+            title: "채팅",
+            image: UIImage(systemName: "bubble.right"),
+            tag: 1
+        )
+        
+        let freindsVC = UIViewController()
+        freindsVC.view.backgroundColor = .white
+        freindsVC.tabBarItem = UITabBarItem(
+            title: "친구",
+            image: UIImage(systemName: "person.3"),
+            tag: 2
+        )
+        
+        let settingVC = UIViewController()
+        settingVC.view.backgroundColor = .white
+        settingVC.tabBarItem = UITabBarItem(
+            title: "설정",
+            image: UIImage(systemName: "person.3"),
+            tag: 3
+        )
+        
+        viewControllers = [postNav, chatVC, settingVC, settingVC]
+    }
+}
---
+++ MorakTests/MorakTests.swift
@@ -0,0 +1,17 @@
+//
+//  MorakTests.swift
+//  MorakTests
+//
+//  Created by Hong jeongmin on 6/30/25.
+//
+
+import Testing
+@testable import Morak
+
+struct MorakTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
